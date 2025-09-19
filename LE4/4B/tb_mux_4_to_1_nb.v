9ecd2ffda06abf910b20c5b5d4fe4e5b4dbf9ade
Joshua Sarcol <jjsnippets@gmail.com>
2025-09-19 10:25:25 +0800

    LE4 in lab

---
+++ LE4/4B/tb_mux_4_to_1_nb.v
@@ -0,0 +1,10 @@
+// CPE 3101L - INTRODUCTION TO HDL
+// Group 1		F 7:30 - 10:30 AM LB285TC
+// LabExercise #4
+// Sarcol, Joshua S.		BS CpE - 3		2025/09/19
+
+// 
+// Testbench file for mux_4_to_1_nb.v (unit test)
+// 
+`timescale 1 ns / 1 ps
+module tb_mux_4_to_1_nb ();
