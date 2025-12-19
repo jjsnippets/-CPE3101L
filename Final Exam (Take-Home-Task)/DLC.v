5331a5ed3712935a4d7e1fe780519829460dfab4
Joshua Sarcol <jjsnippets@gmail.com>
2025-12-19 14:59:31 +0800

    testbench file

--- Final Exam (Take-Home-Task)/DLC.v
+++ Final Exam (Take-Home-Task)/DLC.v
@@ -4,21 +4,24 @@
 // Dotillos, Jeff Thomas; Sarcol, Joshua        BS CpE - 3      2025/12/18
 
 // Digital Lock Controller (DLC)
-// Design a Verilog module for a digital lock controller that accepts two inputs, X and Y, and controls the UNLOCK 
-// actuator output. The module must implement a Finite State Machine (FSM) to detect the specific input 
-// sequence X -> Y -> X -> X -> Y. The logic must be designed to handle variable timing: each step in the sequence 
-// is valid as long as the input is held for at least one clock cycle, and the system must maintain its current state 
-// (without resetting) if both inputs go low for any number of cycles between valid sequence elements. Upon 
-// successful observation of the full sequence, the UNLOCK output should be asserted high and the GREEN light 
-// indicator should also be asserted high. The system must remain in the unlocked state (GREEN light is off) until 
-// a change occurs on either input or when LOCK is asserted: any subsequent transition on X or Y or trigger in 
-// LOCK must immediately cause UNLOCK and GREEN to go low, and reset the detection logic. The nRESET will 
-// also reset the detection logic. 
+// Design a Verilog module for a digital lock controller that accepts two inputs, X and Y,
+// and controls the UNLOCK actuator output. The module must implement a Finite State Machine
+// (FSM) to detect the specific input sequence X -> Y -> X -> X -> Y. The logic must be
+// designed to handle variable timing: each step in the sequence is valid as long as the input
+// is held for at least one clock cycle, and the system must maintain its current state 
+// (without resetting) if both inputs go low for any number of cycles between valid sequence
+// elements. Upon successful observation of the full sequence, the UNLOCK output should be
+// asserted high and the GREEN light indicator should also be asserted high. The system must
+// remain in the unlocked state (GREEN light is off) until a change occurs on either input
+// or when LOCK is asserted: any subsequent transition on X or Y or trigger in LOCK must
+// immediately cause UNLOCK and GREEN to go low, and reset the detection logic. The nRESET
+// will also reset the detection logic. 
 
-module DLC #(parameter oldHz = 50_000_000, newHz = 1, stateView = 0)    (
+module DLC #(parameter oldHz = 50_000_000, newHz = 1, stateView = 1)    (
     input wire          CLK, nRESET, X, Y, LOCK,
     output reg          UNLOCK, GREEN,
-    output wire [3:0]   oCSTATE, oNSTATE                                );
+    output wire [3:0]   oCSTATE, oNSTATE,                                
+    output wire         oclk_div                                        );
     
 // Clock divider logic
     // New divided clock
@@ -134,6 +137,7 @@ module DLC #(parameter oldHz = 50_000_000, newHz = 1, stateView = 0)    (
             default:    begin UNLOCK <= 1'b0; GREEN <= 1'b0; end
         endcase
     assign oCSTATE = (stateView) ? cstate : 4'b000;
-    assign oNSTATE = (stateView) ? nstate : 4'b000;    
+    assign oNSTATE = (stateView) ? nstate : 4'b000;
+    assign oclk_div = (stateView) ? clk_div : 1'b0;
 
 endmodule
